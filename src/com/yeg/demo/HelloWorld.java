20ce565c03302b18cf48626954f9cc7ba54db2ad
yeg <yegking@163.com>
2017-07-03 00:17:28 +0800

    改变第一次

--- src/com/yeg/demo/HelloWorld.java
+++ src/com/yeg/demo/HelloWorld.java
@@ -5,7 +5,7 @@ package com.yeg.demo;
  */
 public class HelloWorld {
     public  static void  main(String[] args){
-        System.out.println("hello,worldw");
+        System.out.println("hello,worldwaaas");
     }
 
 }
