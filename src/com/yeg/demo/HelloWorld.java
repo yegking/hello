78f29fd9c4207539055140f1c631fd3c7443574f
yeg <yegking@163.com>
2017-07-03 00:13:14 +0800

    Initial Commit

---
+++ src/com/yeg/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yeg.demo;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+public class HelloWorld {
+    public  static void  main(String[] args){
+        System.out.println("hello,worldw");
+    }
+
+}
